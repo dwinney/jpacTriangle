d5d8eec322331cb096d16a1d2045efd58b909a99
dwinney <dwinney@iu.edu>
2020-05-29 21:34:31 -0400

    new and imporived executables

--- executables/compare.cpp
+++ executables/compare.cpp
@@ -1,55 +1,54 @@
-// Test code for dispersive triangle with spin
+// Test code for plotting both at the same time
+//
+// Produces .dat and .pdf plotting the triangle for both the feynman evaluation
+// or dispersive evaluation for comparison.
 //
 // Author:       Daniel Winney (2019)
 // Affiliation:  Joint Physics Analysis Center (JPAC)
 // Email:        dwinney@iu.edu
 // ---------------------------------------------------------------------------
 
-#include "quantum_numbers.hpp"
-#include "constants.hpp"
+#include "feynman/feynman_triangle.hpp"
 #include "dispersive/dispersive_triangle.hpp"
-#include "dispersive/projection_function.hpp"
+#include "quantum_numbers.hpp"
 
 #include "jpacGraph1Dc.hpp"
 
-#include <iostream>
-#include <iomanip>
 #include <cstring>
 #include <string>
 #include <ctime>
 
-int main(int argc, char** argv)
+int main( int argc, char** argv )
 {
   // Desired quantum numbers
   int j = 0, jp = 0;
-
+  int Np = 10;
   // Parse inputs
   for (int i = 0; i < argc; i++)
   {
-    if (std::strcmp(argv[i],"-j")==0) j = atof(argv[i+1]);
-    if (std::strcmp(argv[i],"-jp")==0) jp = atof(argv[i+1]);
+    if (std::strcmp(argv[i],"-j")==0) j = atoi(argv[i+1]);
+    if (std::strcmp(argv[i],"-jp")==0) jp = atoi(argv[i+1]);
+    if (std::strcmp(argv[i],"-N")==0) Np = atoi(argv[i+1]);
   }
 
   // All the associated quantum numbers for the amplitude
   quantum_numbers qns;
   qns.n = 1;
+  qns.l = 0;
   qns.j = j;
   qns.jp = jp;
   qns.mDec = .780; // The decaying particle mass
 
-  // Initialize a triangle object passing the above propogator
-  dispersive_triangle tri(&qns);
-
-  projection_function test(&qns);
+  // Initialize a triangle object
+  feynman_triangle tri_feyn(&qns);
+  dispersive_triangle tri_disp(&qns);
 
   // Choose the name for the output files to have (sans and extentions)
-  std::string filename = "omega_disp.pdf";
+  std::string filename = "omega_compare.pdf";
 
   // Plotting bounds
   double low = EPS;
-  double high = 81. * mPi * mPi;
-
-  int Np = 100; // Number of points to plot
+  double high = 81. * mPi2;
 
 // ---------------------------------------------------------------------------
 // You shouldnt need to change anything below this line
@@ -59,41 +58,48 @@ int main(int argc, char** argv)
 // Calculate the triangle function first with the feynman representation
 
   std::cout << "\n";
-  std::cout << "Calculating Dispersive triangle... \n";
+  std::cout << "Calculating Feynman triangle... \n\n";
 
   std::vector<double> s;
-  std::vector< std::complex<double> > disp;
+  std::vector< std::complex<double> > feyn, disp;
 
   clock_t begin = clock();
 
-  for (int i = 0; i < Np; i++)
+  for (int i = 0; i <= Np; i++)
   {
-    double si = low + double(i) * (high - low) / double(Np);
-
-    std::complex<double> fx_d = tri.eval(si, mRho2);
-
+    double si = low + EPS + double(i) * (high - low) / double(Np);
     s.push_back(sqrt(si) / mPi);
+
+    std::complex<double> fx_f = tri_feyn.eval(si, mRho2);
+    feyn.push_back(fx_f);
+
+    std::complex<double> fx_d = tri_disp.eval(si, mRho2);
     disp.push_back(fx_d);
 
     std::cout << std::left;
     std::cout << std::setw(7)  << i;
-    std::cout << std::setw(15) << si;
-    std::cout << std::setw(30) << fx_d << std::endl;
+    std::cout << std::setw(15) << sqrt(si) / mPi;
+    std::cout << std::setw(30) << fx_d;
+    std::cout << std::setw(30) << fx_f;
+    std::cout << std::setw(15) << std::abs(fx_d - fx_f) << std::endl;
   }
 
   clock_t end = clock();
   double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
-  std::cout << "Done in " << elapsed_secs << " seconds. \n";
+  std::cout << "\nDone in " << elapsed_secs << " seconds. \n";
   std::cout << "\n";
 
   jpacGraph1Dc* plotter = new jpacGraph1Dc();
-  plotter->AddEntry(s, disp, "");
-  plotter->SetLegend(false);
+  plotter->AddEntry(s, feyn, "feynman");
+  plotter->AddEntry(s, disp, "dispersive");
+
+  plotter->SetLegend(0.68, 0.75);
 
   plotter->SetXaxis("#sqrt{s} / m_{#pi}", 0, 10);
   plotter->Plot(filename);
 
   delete plotter;
+
   return 1.;
 };
--- executables/feynman_test.cpp
+++ executables/feynman_test.cpp
@@ -1,16 +1,13 @@
-// Test code for feynman triangle with spin
-//
-// Produces .dat and .pdf plotting the triangle for both the feynman evaluation
-// or dispersive evaluation for comparison.
+// Test code for the feynman evaluation
 //
 // Author:       Daniel Winney (2019)
 // Affiliation:  Joint Physics Analysis Center (JPAC)
 // Email:        dwinney@iu.edu
 // ---------------------------------------------------------------------------
 
-#include "breit_wigner.hpp"
 #include "feynman/feynman_triangle.hpp"
-#include "constants.hpp"
+#include "dispersive/dispersive_triangle.hpp"
+#include "quantum_numbers.hpp"
 
 #include "jpacGraph1Dc.hpp"
 
@@ -22,38 +19,32 @@ int main( int argc, char** argv )
 {
   // Desired quantum numbers
   int j = 0, jp = 0;
-
+  int Np = 50;
   // Parse inputs
   for (int i = 0; i < argc; i++)
   {
-    if (std::strcmp(argv[i],"-j")==0) j = atof(argv[i+1]);
-    if (std::strcmp(argv[i],"-jp")==0) jp = atof(argv[i+1]);
+    if (std::strcmp(argv[i],"-j")==0) j = atoi(argv[i+1]);
+    if (std::strcmp(argv[i],"-jp")==0) jp = atoi(argv[i+1]);
+    if (std::strcmp(argv[i],"-N")==0) Np = atoi(argv[i+1]);
   }
 
   // All the associated quantum numbers for the amplitude
   quantum_numbers qns;
   qns.n = 1;
+  qns.l = 0;
   qns.j = j;
   qns.jp = jp;
   qns.mDec = .780; // The decaying particle mass
 
-  // Start a breit_wigner object for rho exchange.
-  // This will serve as our LHC being convoluted in the triangle diagram
-  breit_wigner left_hand_cut(.770, .145);
-
-  // Initialize a triangle object passing the above propogator
-  // and the mass of the decaying particle
-  feynman_triangle tri(&qns, &left_hand_cut);
-
+  // Initialize a triangle object
+  feynman_triangle tri_feyn(&qns);
 
   // Choose the name for the output files to have (sans and extentions)
   std::string filename = "omega_feyn.pdf";
 
   // Plotting bounds
-  double low = 0.;
+  double low = EPS;
   double high = 81. * mPi2;
-
-  int Np = 50; // Number of points to plot
 
 // ---------------------------------------------------------------------------
 // You shouldnt need to change anything below this line
@@ -66,23 +57,23 @@ int main( int argc, char** argv )
   std::cout << "Calculating Feynman triangle... \n\n";
 
   std::vector<double> s;
-  std::vector< std::complex<double> > feyn;
+  std::vector< std::complex<double> > feyn, disp;
 
   clock_t begin = clock();
 
   for (int i = 0; i <= Np; i++)
   {
     double si = low + EPS + double(i) * (high - low) / double(Np);
-    // std::complex<double> fx_f = tri.eval(si);
-    std::complex<double> fx_f = tri.kernel(si, mRho2);
-
     s.push_back(sqrt(si) / mPi);
+
+    std::complex<double> fx_f = tri_feyn.eval(si, mRho2);
     feyn.push_back(fx_f);
 
     std::cout << std::left;
     std::cout << std::setw(7)  << i;
-    std::cout << std::setw(15) << si;
-    std::cout << std::setw(30) << fx_f << std::endl;
+    std::cout << std::setw(15) << sqrt(si) / mPi;
+    std::cout << std::setw(30) << fx_f;
+    std::cout << std::endl;
   }
 
   clock_t end = clock();
@@ -92,7 +83,8 @@ int main( int argc, char** argv )
   std::cout << "\n";
 
   jpacGraph1Dc* plotter = new jpacGraph1Dc();
-  plotter->AddEntry(s, feyn, "");
+  plotter->AddEntry(s, feyn, "feynman");
+
   plotter->SetLegend(false);
 
   plotter->SetXaxis("#sqrt{s} / m_{#pi}", 0, 10);
